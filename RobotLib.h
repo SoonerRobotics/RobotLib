bb02719609c2132be099611d760821bbb33dc196
jkleiber <jkleiber8@gmail.com>
2017-12-16 21:15:30 -0600

    Creates a header file for including everything at once

---
+++ RobotLib.h
@@ -0,0 +1,17 @@
+#ifndef ROBOTLIB_H
+#define ROBOTLIB_H
+
+/**
+ * The purpose of this header file is to be able to include the whole library 
+ * with one include statement.
+ */
+
+#include "AnalogDevice.h"
+#include "BasicDrive.h"
+#include "Collection.h"
+#include "DigitalDevice.h"
+#include "Encoder.h"
+#include "MotorController.h"
+#include "PIDController.h"
+
+#endif
