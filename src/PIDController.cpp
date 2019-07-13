eaaf470b1b333be86d88553da2c494ca06293c1a
Justin Kleiber <jkleiber8@gmail.com>
2019-07-13 17:12:52 -0400

    Makes indentation consistent and adds better comment

--- src/PIDController.cpp
+++ src/PIDController.cpp
@@ -7,7 +7,7 @@ PIDController::PIDController()
 {
     this->last_process_var = 0;
     this->err = 0;
-	this->integrator = 0;
+    this->integrator = 0;
     this->prevErr = 0;
 
     this->kP = 0;
@@ -26,9 +26,9 @@ PIDController::PIDController(float process_init, float kp, float ki, float kd)
     this->last_process_var = process_init;
     this->err = 0;
     this->prevErr = 0;
-	this->integrator = 0;
+    this->integrator = 0;
 
-	this->kP = kp;
+    this->kP = kp;
     this->kI = ki;
     this->kD = kd;
 
@@ -43,39 +43,39 @@ PIDController::PIDController(float process_init, float kp, float ki, float kd)
 */
 void PIDController::operator=(const PIDController& pidController)
 {
-	this->last_process_var = pidController.last_process_var;
-	this->kP = pidController.kP;
-	this->kI = pidController.kI;
-	this->kD = pidController.kD;
-	this->err = pidController.err;
+    this->last_process_var = pidController.last_process_var;
+    this->kP = pidController.kP;
+    this->kI = pidController.kI;
+    this->kD = pidController.kD;
+    this->err = pidController.err;
     this->prevErr = pidController.prevErr;
-	this->integrator = pidController.integrator;
-	this->low = pidController.low;
-	this->high = pidController.high;
+    this->integrator = pidController.integrator;
+    this->low = pidController.low;
+    this->high = pidController.high;
 }
 
 void PIDController::initialize(float process_init, float kp, float ki, float kd)
 {
     this->last_process_var = process_init;
 	
-	this->kP = kp;
+    this->kP = kp;
     this->kI = ki;
     this->kD = kd;
 
     this->err = 0;
     this->integrator = 0;
 	
-	lastTime = millis();
+    lastTime = millis();
 }
 
-/*
-    @param process - current value
-    
-    @param setpoint - target value
 
-    @return P + I + D
-
-*/
+/**
+ * @brief Gets the output from the PID
+ * 
+ * @param setpoint - The target value
+ * @param cur_var - The current value
+ * @return double - The output (p + i + d)
+ */
 float PIDController::getOutput(float setpoint, float process)
 {
     this->err = setpoint - process;
@@ -101,6 +101,6 @@ float PIDController::getOutput(float setpoint, float process)
 
 void PIDController::setOutputRange(float upper, float lower)
 {
-	this->high = upper;
-	this->low = lower;
+    this->high = upper;
+    this->low = lower;
 }
