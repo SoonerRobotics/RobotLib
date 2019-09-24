b7574264113d8c3afbdd4a03b56a11cc9d0aaa46
Noah Zemlin <noah.zemlin@gmail.com>
2019-09-24 12:03:14 -0500

    Fix build errors

--- src/BusInOut.cpp
+++ src/BusInOut.cpp
@@ -1,25 +1,30 @@
 #include "BusInOut.h"
 
-BusInOut::BusInOut(int p0, int p1 = -1, int p2 = -1, int p3 = -1,
-                int p4 = -1, int p5 = -1, int p6 = -1, int p7 = -1,
-                int p8 = -1, int p9 = -1, int p10 = -1, int p11 = -1,
-                int p12 = -1, int p13 = -1, int p14 = -1, int p15 = -1) {
+BusInOut::BusInOut(int p0, int p1, int p2, int p3,
+                int p4, int p5, int p6, int p7,
+                int p8, int p9, int p10, int p11,
+                int p12, int p13, int p14, int p15) {
     
-    _pin = {p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15};
+    int pins[] = {p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15};
+    for (int i=0; i<16; i++) {
+        _pin[i] = pins[i];
+    }
 }
 
-BusInOut::BusInOut(int[]  pins[16]) {
-    _pin = pins;
+BusInOut::BusInOut(int  pins[]) {
+    for (int i=0; i<16; i++) {
+        _pin[i] = pins[i];
+    }
 }
 
-BusInOut::mode(int mode) {
+void BusInOut::mode(int mode) {
     for (int i=0; i<16; i++) {
         if (_pin[i] != -1)
             pinMode(_pin[i], mode);
     }
 }
 
-BusInOut::write(int value) {
+void BusInOut::write(int value) {
     for (int i=0; i<16; i++) {
         if (_pin[i] != -1)
             digitalWrite(_pin[i], (value >> i) && 1);
--- tests/test_businout.cpp
+++ tests/test_businout.cpp
@@ -3,7 +3,7 @@
 
 TEST_CASE("Constructor and mode") 
 {
-    BusInOut test(1,2,4)
+    BusInOut test(1,2,4);
     test.mode(INPUT_PULLUP);
 
     REQUIRE(getModeAtPin(1) == INPUT_PULLUP);
