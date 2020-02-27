50fd2037b3da119ba235f6d1ada477507a95daf4
Noah Zemlin <noah.zemlin@gmail.com>
2020-02-27 11:07:45 -0600

    Upgrade pip in deps

--- install-deps.sh
+++ install-deps.sh
@@ -4,5 +4,5 @@ sudo apt-get install gcc lcov gcc-avr binutils-avr avr-libc -y
 
 # Depdendencies for arduinokeywords
 sudo apt-get install python-pip -y
-pip install wheel
+python -m pip install --upgrade pip setuptools wheel
 pip install arduinokeywords
